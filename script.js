e5ab5cd4f58818e347c4dd21bb327f2e84938440
MrForestino <borv2006@gmail.com>
2024-04-28 20:24:54 +0200

    update

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 new Swiper('.swiper-container',{
 	slidesPerView: 1,
 	spaceBetween: 20,
-	//Стрелки
 	navigation:{
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev'
 	},
-	//Навигация
-	//Буллетыб текущее положениеб прогрессбар
 	pagination:{
 		el: '.swiper-pagination',
-		//Буллеты
 		clickable: true,
 	},
+	initialSlide: 2,
 });
